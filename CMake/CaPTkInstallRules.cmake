e3c085f73ec235fbad59565b34adf3eb793dadce
Dimitris Bounias <bounias.dimitris@gmail.com>
2020-01-30 22:47:11 -0500

    Didn't work either

--- CMake/CaPTkInstallRules.cmake
+++ CMake/CaPTkInstallRules.cmake
@@ -2,4 +2,5 @@
 
 MITK_INSTALL(FILES
   "${CMAKE_CURRENT_LIST_DIR}/../Applications/CaPTkApp/icons/icon.ico"
+  "${CMAKE_CURRENT_LIST_DIR}/../Plugins/org.mitk.cbica.captk.application/resources/cbica-logo.jpg"
 )
