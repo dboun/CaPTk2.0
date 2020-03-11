689f14e1ef324f13148c104b1b523442e63734c4
Dimitris Bounias <bounias.dimitris@gmail.com>
2020-03-10 22:27:22 -0400

    Create helper functions in AGD test

--- Modules/CaPTkCommon/test/CaPTkItkAdaptiveGeodesicDistanceFilterTest.cpp
+++ Modules/CaPTkCommon/test/CaPTkItkAdaptiveGeodesicDistanceFilterTest.cpp
@@ -25,167 +25,117 @@ class CaPTkItkAdaptiveGeodesicDistanceFilterTestSuite : public mitk::TestFixture
     using ImageTypeFloat2D = itk::Image<float, 2>; 
     using ImageTypeInt2D   = itk::Image<int, 2>; 
     using ImageTypeFloat3D = itk::Image<float, 3>;
-    using ImageTypeInt3D   = itk::Image<int, 3>; 
-
-    using DiffFloat2D = itk::Testing::ComparisonImageFilter<ImageTypeFloat2D, ImageTypeFloat2D>;
-    using DiffInt2D   = itk::Testing::ComparisonImageFilter<ImageTypeInt2D, ImageTypeInt2D>;
-    using DiffFloat3D = itk::Testing::ComparisonImageFilter<ImageTypeFloat3D, ImageTypeFloat3D>;
-    using DiffInt3D   = itk::Testing::ComparisonImageFilter<ImageTypeInt3D, ImageTypeInt3D>;
+    using ImageTypeInt3D   = itk::Image<int, 3>;
 
 private:
     std::string CAPTK_DATA_DIR;
 
-    DiffFloat2D::Pointer diffFloat2D;
-    DiffInt2D::Pointer diffInt2D;
-    DiffFloat3D::Pointer diffFloat3D;
-    DiffInt3D::Pointer diffInt3D;
-
     /*---- Input and expected output images ----*/
 
     ImageTypeFloat2D::Pointer   inputImageFloat2D;
-    ImageTypeFloat2D::Pointer   inputImageInt2D;
+    ImageTypeInt2D::Pointer     inputImageInt2D;
     ImageTypeFloat3D::Pointer   inputImageFloat2Das3D;
     ImageTypeFloat3D::Pointer   inputImageFloat3D;
     
     ImageTypeInt2D::Pointer     inputLabelsInt2D;
-    ImageTypeInt2D::Pointer     inputLabelsFloat2D;
-    ImageTypeInt3D::Pointer     inputLabelsFloat2Das3D;
+    ImageTypeFloat2D::Pointer   inputLabelsFloat2D;
+    ImageTypeFloat3D::Pointer   inputLabelsFloat2Das3D;
     ImageTypeInt3D::Pointer     inputLabelsInt3D;
     
     ImageTypeFloat2D::Pointer   outputImageFloat2D;
-    ImageTypeFloat2D::Pointer   outputImageInt2D;
-    ImageTypeFloat3D::Pointer   outputImageInt2Das3D;
+    ImageTypeInt2D::Pointer     outputImageInt2D;
+    ImageTypeInt3D::Pointer     outputImageInt2Das3D;
     ImageTypeFloat3D::Pointer   outputImageFloat3D;
     
     ImageTypeFloat2D::Pointer   maskImageFloat2D;
     ImageTypeFloat3D::Pointer   maskImageFloat3D;
 
-    // template <class TImageType>
-    // TImageType::Pointer ReadImage(std::string path)
-    // {
-    //     using ReaderType = itk::ImageFileReader<TImageType>;
-    //     ReaderType::Pointer reader = ReaderType::New();
-    //     reader->SetFileName(path);
-    //     reader->Update();
-    //     return reader->GetOutput();
-    // }
+    /** \brief Helper function to read an image */
+    template <class TImageType>
+    static typename TImageType::Pointer ReadImage(const std::string& path)
+    {
+        using ReaderType = itk::ImageFileReader<TImageType>;
+        typename ReaderType::Pointer reader = ReaderType::New();
+        reader->SetFileName(path);
+        reader->Update();
+        return reader->GetOutput();
+    }
+
+    /** \brief Helper function to check if every pixel pair of two images are the same */
+    template <class TImageType>
+    static bool Equal(const typename TImageType::Pointer validImage, 
+               const typename TImageType::Pointer testImage)
+    {
+        using DiffType = itk::Testing::ComparisonImageFilter<TImageType, TImageType>;
+        typename DiffType::Pointer diff = DiffType::New();
+        diff->SetToleranceRadius( 0 );
+        diff->SetDifferenceThreshold( 0.001 ); // So that comparing int to floats is fine
+
+        diff->SetValidInput( validImage );
+        diff->SetTestInput( testImage );
+        diff->UpdateLargestPossibleRegion();
+        // averageIntensityDifference kept for reference
+        // const double averageIntensityDifference = 
+        //         diff->GetTotalDifference();
+        const unsigned long numberOfPixelsWithDifferences = 
+                diff->GetNumberOfPixelsWithDifferences();
+
+        return (numberOfPixelsWithDifferences == 0);
+    }
 
 public:
-    void
-    setUp() override
+    void setUp() override
     {
         CAPTK_DATA_DIR = captk::data::absolute_search_dirs[0];
 
-        diffFloat2D = DiffFloat2D::New();
-        diffFloat2D->SetDifferenceThreshold( 0.001 );
-        diffFloat2D->SetToleranceRadius( 0 );
-
-        diffInt2D = DiffInt2D::New();
-        diffInt2D->SetDifferenceThreshold( 0 );
-        diffInt2D->SetToleranceRadius( 0 );
-
-        diffFloat3D = DiffFloat3D::New();
-        diffFloat3D->SetDifferenceThreshold( 0.001 );
-        diffFloat3D->SetToleranceRadius( 0 );
-
-        diffInt3D = DiffInt3D::New();
-        diffInt3D->SetDifferenceThreshold( 0 );
-        diffInt3D->SetToleranceRadius( 0 );
-
         /*---- Read input and expected output images ----*/
-
-        using ReaderTypeFloat2D = itk::ImageFileReader<ImageTypeFloat2D>;
-        using ReaderTypeInt2D = itk::ImageFileReader<ImageTypeInt2D>;
-        using ReaderTypeFloat3D = itk::ImageFileReader<ImageTypeFloat3D>;
-        using ReaderTypeInt3D = itk::ImageFileReader<ImageTypeInt3D>;
-
-        ReaderTypeFloat2D::Pointer reader1  = ReaderTypeFloat2D::New();
-        ReaderTypeFloat2D::Pointer reader2  = ReaderTypeFloat2D::New();
-        ReaderTypeFloat3D::Pointer reader3  = ReaderTypeFloat3D::New();
-        ReaderTypeFloat3D::Pointer reader4  = ReaderTypeFloat3D::New();
-        ReaderTypeInt2D::Pointer   reader5  = ReaderTypeInt2D::New();
-        ReaderTypeInt2D::Pointer   reader6  = ReaderTypeInt2D::New();
-        ReaderTypeInt3D::Pointer   reader7  = ReaderTypeInt3D::New();
-        ReaderTypeInt3D::Pointer   reader8  = ReaderTypeInt3D::New();
-        ReaderTypeFloat2D::Pointer reader9  = ReaderTypeFloat2D::New();
-        ReaderTypeFloat2D::Pointer reader10 = ReaderTypeFloat2D::New();
-        ReaderTypeFloat3D::Pointer reader11 = ReaderTypeFloat3D::New();
-        ReaderTypeFloat3D::Pointer reader12 = ReaderTypeFloat3D::New();
-        ReaderTypeFloat2D::Pointer reader13 = ReaderTypeFloat2D::New();
-        ReaderTypeFloat3D::Pointer reader14 = ReaderTypeFloat3D::New();
-
-        reader1->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D.nii.gz");
-        reader2->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D.nii.gz");
-        reader3->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D.nii.gz");
-        reader4->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_3D/"
-                                              "small3D.nii.gz");
-
-        reader5->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D-labels.nii.gz");
-        reader6->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D-labels.nii.gz");
-        reader7->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_2D/"
-                                              "small2D-labels.nii.gz");
-        reader8->SetFileName(CAPTK_DATA_DIR + "/datasets/ExampleSmall/Subject_3D/"
-                                              "small3D-labels.nii.gz");
-
-        reader9->SetFileName(CAPTK_DATA_DIR + "/test-data/Common/AdaptiveGeodesicDistance/"
-                                              "small2D-agd-result-lb2-limit255-imageasmask.nii.gz");
-        reader10->SetFileName(CAPTK_DATA_DIR + "/test-data/Common/AdaptiveGeodesicDistance/"
-                                               "small2D-agd-result-lb2-limit255-imageasmask.nii.gz");
-        reader11->SetFileName(CAPTK_DATA_DIR + "/test-data/Common/AdaptiveGeodesicDistance/"
-                                               "small2D-agd-result-lb2-limit255-imageasmask.nii.gz");
-        reader12->SetFileName(CAPTK_DATA_DIR + "/test-data/Common/AdaptiveGeodesicDistance/"
-                                               "small3D-agd-result-lb1-limit255-imageasmask.nii.gz");
-        // TODO: reader13 (also uncomment stuff below)
-        // TODO: reader14 (also uncomment stuff below)
 
         try
         {
-            reader1->Update();
-            reader2->Update();
-            reader3->Update();
-            reader4->Update();
-            reader5->Update();
-            reader6->Update();
-            reader7->Update();
-            reader8->Update();
-            reader9->Update();
-            reader10->Update();
-            reader11->Update();
-            reader12->Update();
-            //reader13->Update();
-            //reader14->Update();
+            inputImageFloat2D = ReadImage<ImageTypeFloat2D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D.nii.gz"));
+            inputImageInt2D = ReadImage<ImageTypeInt2D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D.nii.gz"));
+            inputImageFloat2Das3D = ReadImage<ImageTypeFloat3D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D.nii.gz"));
+            inputImageFloat3D = ReadImage<ImageTypeFloat3D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_3D/small3D.nii.gz"));
+
+            inputLabelsInt2D = ReadImage<ImageTypeInt2D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D-labels.nii.gz"));
+            inputLabelsFloat2D = ReadImage<ImageTypeFloat2D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D-labels.nii.gz"));
+            inputLabelsFloat2Das3D = ReadImage<ImageTypeFloat3D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_2D/small2D-labels.nii.gz"));
+            inputLabelsInt3D = ReadImage<ImageTypeInt3D>(CAPTK_DATA_DIR 
+                    + std::string("/datasets/ExampleSmall/Subject_3D/small3D-labels.nii.gz"));
+
+            outputImageFloat2D = ReadImage<ImageTypeFloat2D>(CAPTK_DATA_DIR 
+                    + std::string("/test-data/Common/AdaptiveGeodesicDistance/"
+                      "small2D-agd-result-lb2-limit255-imageasmask.nii.gz"));
+            outputImageInt2D = ReadImage<ImageTypeInt2D>(CAPTK_DATA_DIR 
+                    + std::string("/test-data/Common/AdaptiveGeodesicDistance/"
+                      "small2D-agd-result-lb2-limit255-imageasmask.nii.gz"));
+            outputImageInt2Das3D = ReadImage<ImageTypeInt3D>(CAPTK_DATA_DIR 
+                    + std::string("/test-data/Common/AdaptiveGeodesicDistance/"
+                      "small2D-agd-result-lb2-limit255-imageasmask.nii.gz"));
+            outputImageFloat3D = ReadImage<ImageTypeFloat3D>(CAPTK_DATA_DIR 
+                    + std::string("/test-data/Common/AdaptiveGeodesicDistance/"
+                      "small3D-agd-result-lb1-limit255-imageasmask.nii.gz"));
+
+            // TODO: maskImageFloat2D (also uncomment stuff below)
+            // TODO: maskImageFloat3D (also uncomment stuff below)
         }
         catch (const itk::ExceptionObject &err)
         {
             CPPUNIT_FAIL(std::string(
                              "Reading on of the input images failed. "
-                             "This might be because you are on an older version of the CaPTkData. "
-                             "If that is the case, please rebuild your superbuild. Exception was: ") +
+                             "This might be because you are on an older "
+                             "version of the CaPTkData. "
+                             "If that is the case, please rebuild your superbuild. "
+                             "Exception was: ") +
                          err.what());
         }
-
-        inputImageFloat2D = reader1->GetOutput();
-        inputImageInt2D = reader2->GetOutput();
-        inputImageFloat2Das3D = reader3->GetOutput();
-        inputImageFloat3D = reader4->GetOutput();
-
-        inputLabelsInt2D = reader5->GetOutput();
-        inputLabelsFloat2D = reader6->GetOutput();
-        inputLabelsFloat2Das3D = reader7->GetOutput();
-        inputLabelsInt3D = reader8->GetOutput();
-
-        outputImageFloat2D = reader9->GetOutput();
-        outputImageInt2D = reader10->GetOutput();
-        outputImageInt2Das3D = reader11->GetOutput();
-        outputImageFloat3D = reader12->GetOutput();
-
-        //maskImageFloat2D = reader13->GetOutput();
-        //maskImageFloat3D = reader14->GetOutput();
     }
 
     void tearDown() override
@@ -195,27 +145,6 @@ public:
 
     void Try2D()
     {
-        // /*---- Read image ----*/
-
-        // using ReaderType = itk::ImageFileReader<ImageTypeFloat2D>;
-        // ReaderType::Pointer reader = ReaderType::New();
-        // reader->SetFileName(CAPTK_DATA_DIR 
-        //     + "/test-data/Common/AdaptiveGeodesicDistance/"
-        //     "small2D-agd-result-lb2-limit255-imageasmask.nii.gz");
-        
-        // try
-        // {
-        //     reader->Update();
-        // }
-        // catch (const itk::ExceptionObject &err)
-        // {
-        //     CPPUNIT_FAIL(std::string(
-        //                  "Reading the input image failed. "
-        //                  "This might be because you are on an older version of the CaPTkData. "
-        //                  "If that is the case, please rebuild your superbuild. Exception was: ") + 
-        //              err.what());
-        // }
-
         /*---- Run filter ----*/
 
         using FilterType = 
@@ -239,23 +168,9 @@ public:
 
         /*---- Compare output image with the "ground truth" ----*/
 
-        diffFloat2D->SetValidInput( outputImageFloat2D );
-        diffFloat2D->SetTestInput( filter->GetOutput() );
-        diffFloat2D->UpdateLargestPossibleRegion();
-        // const double averageIntensityDifference = 
-        //         diffFloat2D->GetTotalDifference();
-        const unsigned long numberOfPixelsWithDifferences = 
-                diffFloat2D->GetNumberOfPixelsWithDifferences();
-
-        CPPUNIT_ASSERT_MESSAGE("Should be equal", numberOfPixelsWithDifferences == 0);
-
-        // CPPUNIT_ASSERT_MESSAGE(
-        //     "Always passes",
-        //     true);
-
-        //... mitk::Equal() ??
-
-        // CPPUNIT_ASSERT_MESSAGE("Should be equal", ref->Equals(outFib));
+        CPPUNIT_ASSERT_MESSAGE( "Valid/Test image equality", 
+                Equal<ImageTypeFloat2D>(outputImageFloat2D, filter->GetOutput())
+        );
     }
 };
 
